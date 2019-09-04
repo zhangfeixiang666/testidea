7921e459c66577bf0a7b3fb6e05e54d9abb7d558
feixiangzhang <3502223206@qq.com>
2019-09-04 19:21:25 +0800

    把项目添加到git

---
+++ git-project/src/cn/asiainfo/FirstDemo.java
@@ -0,0 +1,18 @@
+package cn.asiainfo;
+
+/**
+ * @Author zhangfeixiang
+ * @Date 2019/9/4 19:12
+ * @Version 1.0
+ */
+public class FirstDemo {
+
+	public static void main(String[] args) {
+
+		System.out.println("args = " + args);
+		System.out.println("66666666");
+		System.out.println("66666666");
+		System.out.println("66666666");
+		System.out.println("77777777");
+	}
+}
